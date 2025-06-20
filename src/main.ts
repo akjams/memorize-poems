816c5ddb4ec8bcdcae99de73d69f6159bb4bc7f6
ak <you@example.com>
2025-06-20 11:11:44 -0700

    print area

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,7 @@ function generatePrintable(poem: string) {
     // create right cell with first letters spaced
     const right = document.createElement('div')
     right.className = 'poemLine hintText'
-    right.textContent = words.map(w => w[0] ?? '').join(' ')
+    right.textContent = words.map(w => w.toLowerCase()[0] ?? '').join(' ')
 
     poemGrid.append(left, right)
   }
@@ -35,23 +35,35 @@ function generatePrintable(poem: string) {
 
 // Main
 
-poemInput.value = `Banish wisdom, discard knowledge
-and the people shall profit a hundredfold;
-banish humanity, discard justice
-and the people shall recover love of their kin
-banish cunning, discard utility
-and the thieves and brigands shall disappear
-as these three touch the externals and are inadequate
-the people have need of what they can depend upon:
-Reveal thy simple self`
+poemInput.value = `If we didn’t have birthdays, you wouldn’t be you.
+If you’d never been born, well then what would you do?
+If you’d never been born, well then what would you be?
+You might be a fish! Or a toad in a tree!
+
+You might be a doorknob! Or three baked potatoes!
+You might be a bag full of hard green tomatoes.
+Or worse than all that… Why, you might be a WASN’T!
+A Wasn’t has no fun at all. No, he doesn’t.
+A Wasn’t just isn’t. He just isn’t present.
+But you…You ARE YOU! And, now isn’t that pleasant!
+
+Today you are you! That is truer than true!
+There is no one alive who is you-er than you!
+Shout loud, “I am lucky to be what I am!
+Thank goodness I’m not just a clam or a ham
+Or a dusty old jar of sour gooseberry jam!
+I am what I am! That’s a great thing to be!
+If I say so myself, HAPPY BIRTHDAY TO ME!"
+`
 
 poemForm.addEventListener('submit', (e: SubmitEvent) => {
 	e.preventDefault()
 	const poemText: string = poemInput.value.trim()
-	console.log('submitted poem:', poemText)
+	// console.log('submitted poem:', poemText)
 	generatePrintable(poemText)
 
 	// TODO: generate printable poem + hint view here
+	// these are links to the same site with QUERY PARAMS of the poem
 })
 
 poemInput.addEventListener('keydown', (e: KeyboardEvent) => {
